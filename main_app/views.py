2b84254b4d3165e7bc95ed533950d00a5b9b3564
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-09-10 14:36:16 -0400

    created index template with hard-coded data

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-		return HttpResponse('<h1>Hello Explorers!</h1>')
+		name = 'Gold Nugget'
+		value = 1000.00
+		context = {'treasure_name': name,
+							 'treasure_val': value }
+		return render(request, 'index.html', context)
